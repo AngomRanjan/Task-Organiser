a3fa11d48021c061e93dbd97186446dc0cc755f3
AngomRanjan <angomranjan@gmail.com>
2022-06-08 19:52:05 +0530

    Remove Hard-Coded Task List

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import { Task, addTask, arrTasks } from './modules/tasksList.js';
 
 populateTaskList();
 
-// const n = Task('hnhvnv');
-
 const textInput = document.getElementById('new-task');
 
 textInput.addEventListener('change', (event) => {
   event.preventDefault();
-  console.log('Hi I am Text input Handler');
   if (textInput.value.trim() === '') {
     alert('Task Cannot be Empty \n Tasks Not added');
   } else {
--- src/modules/tasksList.js
+++ src/modules/tasksList.js
@@ -1,27 +1,6 @@
-const arrTasks = [
-  {
-    index: 1,
-    description: 'Join Morning Group Session',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'Join Morning Program Session',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'Join Evening Program Session',
-    completed: false,
-  },
-  {
-    index: 4,
-    description: 'Join Evening Stand-Up',
-    completed: false,
-  },
-];
+const arrTasks = [];
 
-const Task = (description) => ({ index: arrTasks.length + 1, description, completed: false });
+const Task = (description) => ({ index: arrTasks.length + 1 || 1, description, completed: false });
 const addTask = (task) => {
   arrTasks.push(task);
 };
