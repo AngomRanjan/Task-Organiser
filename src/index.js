fc905a51479e2e3995bfb89075e1f138bbe9298c
AngomRanjan <angomranjan@gmail.com>
2022-06-08 19:27:06 +0530

    Generate Updated Tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import './style.css';
-import { populateTaskList } from './modules/taskItem.js';
+import { populateTaskList, resetTasks } from './modules/taskItem.js';
 import { Task, addTask, arrTasks } from './modules/tasksList.js';
 
 populateTaskList();
 
 // const n = Task('hnhvnv');
-addTask(Task('hnhvnv'));
-console.table(arrTasks);
 
 const textInput = document.getElementById('new-task');
 
@@ -15,6 +13,11 @@ textInput.addEventListener('change', (event) => {
   console.log('Hi I am Text input Handler');
   if (textInput.value.trim() === '') {
     alert('Task Cannot be Empty \n Tasks Not added');
+  } else {
+    addTask(Task(textInput.value.trim()));
+    console.table(arrTasks);
+    resetTasks();
+    populateTaskList();
   }
   textInput.value = '';
   textInput.blur();
--- src/modules/tasksList.js
+++ src/modules/tasksList.js
@@ -1,27 +1,27 @@
 const arrTasks = [
   {
-    index: 0,
+    index: 1,
     description: 'Join Morning Group Session',
     completed: false,
   },
   {
-    index: 1,
+    index: 2,
     description: 'Join Morning Program Session',
     completed: false,
   },
   {
-    index: 2,
+    index: 3,
     description: 'Join Evening Program Session',
     completed: false,
   },
   {
-    index: 3,
+    index: 4,
     description: 'Join Evening Stand-Up',
     completed: false,
   },
 ];
 
-const Task = (description) => ({ index: arrTasks.length, description, completed: false });
+const Task = (description) => ({ index: arrTasks.length + 1, description, completed: false });
 const addTask = (task) => {
   arrTasks.push(task);
 };
