7487c4f82503dfc17456193676ec92586cb40691
AngomRanjan <angomranjan@gmail.com>
2023-09-01 20:12:40 +0530

    Refactor ArrTasks class

--- src/modules/to_class.js
+++ src/modules/to_class.js
@@ -1,42 +1,57 @@
 // || =============== Tasks Class ================== ||
 class ArrTasks {
-  constructor() {
-    this.tasks = [];
+  #tasks;
+  constructor(list) {
+    this.#tasks = list || [];
   }
+
+  get tasks() {
+    return this.#tasks;
+  }
+
+  get length() {
+    return this.#tasks.length;
+  }
+
+  get lastItem() {
+    return this.#tasks[this.length-1];
+  }
+
+  isExist(desc) {
+    return this.#tasks.find(item => item.description === desc);
+  };
 
   newTask(description) {
     return {
-      index: this.tasks.length + 1 || 1,
-      description,
+      index: this.length + 1,
+      description: description.trim(),
       completed: false,
     };
   }
 
   updateIndex() {
-    this.tasks = this.tasks.map((task, i) => { task.index = i + 1; return task; });
+    this.#tasks = this.#tasks.map((task, i) => { task.index = i + 1; return task; });
   }
 
   addTask(description) {
-    this.tasks.push(this.newTask(description));
+    this.#tasks.push(this.newTask(description));
   }
 
-  deleteTask(arrIndex) {
-    this.tasks.splice(arrIndex, 1);
+  deleteTask(index) {
+    this.#tasks.splice(index, 1);
     this.updateIndex();
   }
 
   clearCompleted() {
-    this.tasks = this.tasks.filter((t) => t.completed === false);
+    this.#tasks = this.#tasks.filter((t) => t.completed === false);
     this.updateIndex();
   }
 
   lastAdded() {
-    return this.tasks[this.tasks.length - 1];
+    return this.#tasks[this.#tasks.length - 1];
   }
-}
+};
 
-const arrTasks = new ArrTasks();
-
-arrTasks.tasks = JSON.parse(localStorage.getItem('arrTaskLocal')) || [];
+const arrTasks =new ArrTasks(JSON.parse(localStorage.getItem('arrTaskLocal')));
 
 export default arrTasks;
