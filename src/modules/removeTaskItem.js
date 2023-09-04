85ef8d8f7731e0b5203ed8f36b57405dc5350b75
AngomRanjan <angomranjan@gmail.com>
2023-09-04 13:04:45 +0530

    Refactor removeTaskItem

--- src/modules/removeTaskItem.js
+++ src/modules/removeTaskItem.js
@@ -1,25 +1,23 @@
 import arrTasks from './to_class.js';
 import saveLocal from './saveLocal.js';
 
-const updateElementId = (firstItem, lastItem) => {
-  for (let i = firstItem; i <= lastItem; i += 1) {
-    const li = document.getElementById(`task-${i}`);
-    const inputText = document.getElementById(`desc-${i}`);
-    const chkBox = document.getElementById(`${i}`);
-    const icon = document.getElementById(`icon-${i}`);
-    li.id = `task-${i - 1}`;
-    inputText.id = `desc-${i - 1}`;
-    chkBox.id = `${i - 1}`;
-    icon.id = `icon-${i - 1}`;
-  }
+const updateElementId = () => {
+  const items =Array.from(document.querySelectorAll('#ul-tasks > li'));
+  items.map((li, i) => (
+    li.id = `task-${i + 1}`,
+    li.children[0].id = i + 1,
+    li.children[1].id =`desc-${i + 1}`,
+    li.children[2].id =`icon-${i + 1}`,
+    {...li}
+  ));
 };
 
 const removeTaskItem = (taskLi) => {
   const eID = taskLi.id.split('-')[1];
   arrTasks.deleteTask(eID - 1);
   saveLocal();
-  taskLi.parentNode.removeChild(taskLi);
-  updateElementId(Number(eID) + 1, arrTasks.tasks.length + 1);
+  taskLi.remove();
+  updateElementId();
 };
 
 export default removeTaskItem;
