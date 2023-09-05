82493abf32bd36df1c9fc3ffb273abde662575c0
AngomRanjan <angomranjan@gmail.com>
2023-09-06 01:09:40 +0530

    Create Utilities.js -- Add newNode function

---
+++ src/modules/utilities.js
@@ -0,0 +1,14 @@
+const newNode = (paramElement, ...paramAttribs) => {
+  // create a new dom element with specified attributes
+  
+  const node = document.createElement(paramElement);
+  if(paramAttribs) {
+    paramAttribs.forEach(attrib => {
+      const [name, value] = attrib.split('=');
+      node[name] = value;
+    });
+  };
+  return node;
+};
+
+export default newNode;
